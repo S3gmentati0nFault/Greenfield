961ab304f9a2e875781364693f012b5f7196f0ca
S3gmentati0nFault <alessandro.biagiotti@studenti.unimi.it>
2023-06-08 17:13:05 +0200

    Erased a newline

--- src/main/java/cleaningBot/CleaningBot.java
+++ src/main/java/cleaningBot/CleaningBot.java
@@ -4,7 +4,6 @@ import beans.BotIdentity;
 import cleaningBot.threads.Bot;
 
 public class CleaningBot extends BotIdentity {
-
     public static void main(String[] args) {
         Bot thread = new Bot();
         thread.run();
